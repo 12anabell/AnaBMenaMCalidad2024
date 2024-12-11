cd5df0b0d3da42b3c21c256380e1146e92c587b0
12anabell <94195910+12anabell@users.noreply.github.com>
2024-12-11 12:22:20 -0600

    Update LoginServiceTest.java

--- src/test/java/com/fca/calidad/servicio/LoginServiceTest.java
+++ src/test/java/com/fca/calidad/servicio/LoginServiceTest.java
@@ -1,44 +1,44 @@
 package com.fca.calidad.servicio;
+ 
+ 
 import static org.mockito.Mockito.mock;
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
+ 
 import com.fca.calidad.dao.IDAOUser;
 import com.fca.calidad.modelo.User;
-
+ 
 class LoginServiceTest {
-		LoginService service;
-		IDAOUser dao;
-	
+
+ 
+	LoginService service;
+	IDAOUser dao;
 	@Test
-	void loginusuarioinexistentetest() {
-		/*Inicialización*/
-		dao = mock(IDAOUser.class);
-		service =  new LoginService (dao);
-		//User usuario = new User("nombre1", "email@email.com", "123456");
-		when(dao.findByUserName("nombre1")).thenReturn(null);
-		
-		/*Ejercicio*/
-		boolean result = service.login("nombre1", "123456");
-		
-		/*Resultado*/
-		assertThat(result, is(false));
-		
-	}
-	@Test
-	void loginPasswordincorrectotest() {
-		/*Inicialización*/
-		dao = mock(IDAOUser.class);
-		service =  new LoginService (dao);
-		User usuario = new User("nombre1", "email@email.com", "123456");
+	void LoginServicetest() {
+		//inicializacion
+		dao = mock(IDAOUser.class); //IDAOUser es la clase a la que se va a reemplazar (dao al ser un mock esta vacio)
+		service = new LoginService(dao); //instancia de la clase a probar
+		User usuario= new User("nombre1","email@email.com", "12345");//crear usuario
 		when(dao.findByUserName("nombre1")).thenReturn(usuario);
-		
-		/*Ejercicio*/
-		boolean result = service.login("nombre1", "123456");
-		
-		/*Resultado*/
-		assertThat(result, is(false));
-		
+		//Ejercicio (llamada)
+		boolean result= service.login("nombre1", "12345");
+		//verificacion
+		assertThat(result, is(true));
 	}
+	@Test
+	void LoginNoExisteUsuariotest() {
+		//inicializacion
+		dao = mock(IDAOUser.class); //IDAOUser es la clase a la que se va a reemplazar (dao al ser un mock esta vacio)
+		service = new LoginService(dao); //instancia de la clase a probar
+		//User usuario= new User("nombre1","email@email.com", "12345");//crear usuario
+		when(dao.findByUserName("nombre1")).thenReturn(null);
+		//Ejercicio (llamada)
+		boolean result= service.login("nombre1", "12345");
+		//verificacion
+		assertThat(result, is(false));
+	}
+
+ 
 }
